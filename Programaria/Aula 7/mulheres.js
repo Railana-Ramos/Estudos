e91015c7a98d53de92c5230e3dfd784009dd2fad
Railana Ramos <railanaramos@gmail.com>
2023-09-18 17:34:31 -0300

    Atualizando_Atividaides

--- Programaria/Aula 7/mulheres.js
+++ Programaria/Aula 7/mulheres.js
@@ -1,20 +1,34 @@
-const express = require("express");
+const express = require("express")
 const router = express.Router();
 
 const app = express()
 const porta = 3333;
 
-function mostraMulher(request, response){
-    response.json({
+const mulheres = [
+    {
         nome: 'Railana Ramos',
         imagem: 'https://github.com/Railana-Ramos.png',
         minibio: 'Estudante e Desenvolvedora'
-    })
+    },
+    {
+        nome: 'Iana Chan',
+        imagem: 'https://pbs.twimg.com/profile_images/1420905428452524037/DURvAmKN_400x400.jpg',
+        minibio: 'Fundadora da Programaria'
+    },
+    {
+        nome: 'Nina da Hora',
+        imagem: 'https://www.fundacaotelefonicavivo.org.br/wp-content/webp-express/webp-images/uploads/2022/11/10-ninadahora1200x628desktop.png.webp?x48208',
+        minibio: 'Hacker antirracista'
+    }
+]
+
+function mostraMulheres (request, response){
+    response.json (mulheres);
 }
 
 function mostraPorta() {
     console.log('Servidor criado e rodando na porta', porta);
 }
 
-app.use(router.get('/mulher', mostraMulher));
+app.use(router.get('/mulheres', mostraMulheres))
 app.listen(porta,mostraPorta);
